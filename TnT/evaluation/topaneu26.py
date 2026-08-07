abbe4972be12ea55c469d1281a1d4062236c603f
Lorenz Achim Kuhn <l.a.kuhn@tue.nl>
2026-08-07 16:46:20 +0200

    Begin work on pipeline and evaluator

--- TnT/evaluation/topaneu26.py
+++ TnT/evaluation/topaneu26.py
@@ -1,5 +1,362 @@
+import numpy as np
+import json, os, math, glob
+from pathlib import Path
+import SimpleITK as sitk
+from pprint import pprint
+from typing import Union, List, Tuple, Literal
+from scipy.ndimage import label, binary_erosion
+from scipy.spatial import cKDTree
 
+N_CLASSES = 52
+
+### copied from main
+def load_image_file_as_array(location: Path) -> np.ndarray:
+    """Load an image as a numpy array using sitk
+
+    Args:
+        location (Path): Path to the image
+
+    Returns:
+        np.ndarray: The image as a 3d array
+    """
+    result = sitk.ReadImage(location)
+    # Convert it to a Numpy array
+    return sitk.GetArrayFromImage(result)
+
+def load_gt(fn: str) -> np.ndarray:
+    """Load a ground truth reference image for a given filepath
+
+    Args:
+        fn (str): The filename of the image
+
+    Returns:
+        np.ndarray: The correpsonding gt segmentation as a 3d array
+    """
+    dir = Path("/opt/ml/input/data/ground_truth/location_masks")
+    return load_image_file_as_array(dir/fn.replace("_0000.mha", ".mha"))
+
+def iou_fn(img1: np.ndarray, img2: np.ndarray, eps: float=1e-6) -> float:
+    """Compute the iou between two binary images
+
+    Args:
+        img1 (np.ndarray): Image a
+        img2 (np.ndarray): Image b
+        eps (float, optional): Epsilon to avoid div by zero error. Defaults to 1e-6.
+
+    Returns:
+        float: The iou between a and b
+    """
+    if not np.any(img1) and not np.any(img2): return 1 # both 0 is perfect seg
+    elif not np.any(img1) or not np.any(img2): return 0 # only one zero is the worst possible segmentation
+    return (np.bitwise_and(img1, img2).sum()/(np.bitwise_or(img1, img2).sum())+eps)
+
+def dice_fn(img1: np.ndarray, img2: np.ndarray, eps: float=1e-6) -> float:
+    """Compute the dice score between two binary images
+
+    Args:
+        img1 (np.ndarray): Image a
+        img2 (np.ndarray): Image b
+        eps (float, optional): Epsilon to avoid div by zero error. Defaults to 1e-6.
+
+    Returns:
+        float: The dice score between a and b
+    """
+    if not np.any(img1) and not np.any(img2): return 1 # both 0 is perfect seg
+    elif not np.any(img1) or not np.any(img2): return 0 # only one zero is the worst possible segmentation
+    return ((2*np.bitwise_and(img1, img2).sum())/(img1.sum() + img2.sum() + eps))
+
+def volsim_fn(img1: np.ndarray, img2: np.ndarray, eps: float=1e-6) -> float: # as described in https://pmc.ncbi.nlm.nih.gov/articles/PMC4533825/pdf/12880_2015_Article_68.pdf
+    """Compute the volumetric similarity between two binary images
+
+    Args:
+        img1 (np.ndarray): Image a
+        img2 (np.ndarray): Image b
+        eps (float, optional): Epsilon to avoid div by zero error. Defaults to 1e-6.
+
+    Returns:
+        float: The volumetric similarity between a and b
+    """
+    if not np.any(img1) and not np.any(img2): return 1 # both 0 is perfect seg
+    elif not np.any(img1) or not np.any(img2): return 0 # only one zero is the worst possible segmentation
+    return 1 - (abs(img1.sum()-img2.sum())/(img1.sum()+img2.sum()+eps))
+
+def get_surface(img: np.ndarray) -> np.ndarray:
+    """Get the surface of the objects in a 3d binary image
+
+    Args:
+        img (np.ndarray): A 3d binary image
+
+    Returns:
+        np.ndarray: A 3d binary image containing only the surface of the input image
+    """
+    return img & ~binary_erosion(img)
+
+def hd_fn(img1: np.ndarray, img2: np.ndarray, perc: int=95, norm: bool=True) -> float: # NOTE this function is claude generated as my implementation with a double loop was too slow.
+    """Compute the percentile of the hausdorff distance between the surfaces of two objects in 3d space
+        In case of either mask being nonzero with the respective other being zero, will return the diagonal of the volume
+
+    Args:
+        img1 (np.ndarray): Image a
+        img2 (np.ndarray): Image b
+        perc (int, optional): The percentile. Defaults to 95.
+        norm (bool, optional): Whether to return absolute or normalized values. Normalized by the diagnoal of the volume. Defaults to True.
+
+    Returns:
+        float: The perc percentile of the bidirectional HD between a and b
+    """
+    diag = np.linalg.norm(img1.shape)
+    if not np.any(img1) and not np.any(img2): return 0 # both 0 is perfect seg
+    elif not np.any(img1) or not np.any(img2): return 1 if norm else diag # only one zero is the worst possible segmentation
+        
+    coords_a = np.argwhere(get_surface(img1))
+    coords_b = np.argwhere(get_surface(img2))
+
+    tree_b = cKDTree(coords_b)
+    tree_a = cKDTree(coords_a)
+
+    d_ab, _ = tree_b.query(coords_a)   # a -> nearest b
+    d_ba, _ = tree_a.query(coords_b)   # b -> nearest a
+    
+    hd = max(np.percentile(d_ab, perc),
+               np.percentile(d_ba, perc))
+
+    return hd/diag if norm else hd
+
+def extended_label(img: np.ndarray) -> Tuple[List[np.ndarray], List[int], int]:
+    """Get the connected components and labels for every class in a segmentation mask
+
+    Args:
+        img (np.ndarray): The segmentation mask
+
+    Returns:
+        Tuple[List[np.ndarray], List[int], int]: The connceted components for each class, the n components for each class and the total number of components in the image.
+    """
+    ccs = []
+    ns = []
+    present_cls = np.unique(img).tolist()
+    for cls in range(1, N_CLASSES+1):
+        if cls not in present_cls:
+            ccs.append(None)
+            ns.append(0)
+        else:
+            cc, n = label(img==cls)
+            ccs.append(cc)
+            ns.append(n)
+    return ccs, ns, sum(ns)
+
+def check_tp(img1: np.ndarray, img2: np.ndarray, threshold: float=0.3, method: Literal["intersection", "dice", "iou"]="intersection") -> bool:
+    """Check if two segmentations are a true positive
+
+    Args:
+        img1 (np.ndarray): Segmentation a
+        img2 (np.ndarray): Segmentation b
+        threshold (float, optional): The threshold to determin a tp. Defaults to 0.3.
+        method (Literal[&quot;intersection&quot;, &quot;dice&quot;, &quot;iou&quot;], optional): The metric to consider. Defaults to "intersection".
+
+    Raises:
+        ValueError: If the passed method is not supported
+
+    Returns:
+        bool: Whether the two input segmentation fulfill the TP criterion.
+    """
+    match method:
+        case "intersection":
+            return np.bitwise_and(img1, img2).sum() > threshold
+        case "dice":
+            return dice_fn(img1, img2) > threshold
+        case "iou":
+            return iou_fn(img1, img2) > threshold
+        case _:
+            raise ValueError(f"Invalid tp checking method '{method}'")
+
+def evaluation_function_instance_level(predictions: np.ndarray, filename: str) -> dict: # numpy array of the predictions and the filename 
+    """Computes the amount of TP, FP, TN, FN and the Dice Score, Volumetric Similarity and HD95 for the TPs for all possible classes for a given sample.
+
+    Args:
+        predictions (np.ndarray): The predicted segmentation
+        filename (str): The name of the corresponding image
+
+    Returns:
+        dict: The metrics.
+    """
+    gts = load_gt(filename)
+    gt_ccs, gt_n_per_aneu, gt_n_aneu = extended_label(gts)
+    pred_ccs, pred_n_per_aneu, pred_n_aneu = extended_label(predictions)
+    
+    results = {}
+    for cls in range(1, N_CLASSES+1):
+        gt = gt_ccs[cls-1]
+        pred = pred_ccs[cls-1]
+        
+        # eval seg per case
+        dsc = 0
+        hd95 = 0
+        vs = 0
+
+        # eval cls performance per instance
+        tp = 0
+        fp = 0
+        if pred_n_per_aneu[cls-1]>0 and gt_n_per_aneu[cls-1]>0: # if both are nonzero do the loop and look for matches
+            for i in range(1,pred_n_per_aneu[cls-1]+1):
+                is_tp = False
+                for j in range(1,gt_n_per_aneu[cls-1]+1):
+                    if check_tp(gt==j, pred==i, threshold=0, method="intersection"):
+                        hd95 += hd_fn(gt==j, pred==i, 95)
+                        vs += volsim_fn(gt==j, pred==i) 
+                        dsc += dice_fn(gt==j, pred==i)
+                        is_tp = True
+                        break
+                if is_tp: tp += 1
+                else: fp += 1
+            fn = gt_n_per_aneu[cls-1]-tp
+            tn = gt_n_aneu-(tp+fn)
+            
+        elif pred_n_per_aneu[cls-1]>0 and gt_n_per_aneu[cls-1]==0: # if no samples of the class available in the gt but there are any in the predictions, count everything as false positive, but also add the true negatives for the other classes
+            fp = pred_n_per_aneu[cls-1]
+            fn = 0
+            tn = gt_n_aneu
+            
+        elif pred_n_per_aneu[cls-1]==0 and gt_n_per_aneu[cls-1]>0: # if no samples of the class available in the predictions but there are any in the gt, count everything as false negative, but also add the true negatives for the other classes
+            fn = gt_n_per_aneu[cls-1]
+            tn = gt_n_aneu-fn
+            
+        else: # If there are no objects of a class in neither gt nor predictions, count only true negatives
+            fn = 0
+            tn = gt_n_aneu
+
+        results[f"DICE_{cls}"] = dsc
+        results[f"HD95_{cls}"] = hd95
+        results[f"VOLSIM_{cls}"] = vs
+        results[f"TP_{cls}"] = tp
+        results[f"FP_{cls}"] = fp
+        results[f"FN_{cls}"] = fn
+        results[f"TN_{cls}"] = tn
+    return results
+
+def evaluation_function(predictions: np.ndarray, gt: np.ndarray) -> dict:
+    """Computes the amount of TP, FP, TN, FN and the Dice Score, Volumetric Similarity and HD95 for the TPs for all possible classes for a given sample.
+
+    Args:
+        predictions (np.ndarray): The predicted segmentation
+        filename (str): The name of the corresponding image
+
+    Returns:
+        dict: The metrics.
+    """
+    
+    assert predictions.shape == gt.shape, f"Shape missmatch: GT={gt.shape}; Prediction={predictions.shape}"
+    
+    results = {}
+    gts = [elem for elem in np.unique(gt).tolist() if elem != 0]
+    n_aneu = len(gts)
+
+    for cls in range(1, N_CLASSES+1): 
+        ## check for presence and tp
+        is_in_pred = np.any(predictions==cls)
+        is_in_gt = np.any(gt==cls)
+        is_tp = check_tp(gt==cls, predictions==cls, 0, "intersection") if (is_in_gt and is_in_pred) else False
+        
+        ## comp tpfptnfn
+        tp = 1 if is_tp else 0
+        fp = 1 if (is_in_pred and not is_tp) else 0
+        fn = 1 if (is_in_gt and not is_tp) else 0
+        tn = n_aneu-(tp+fn)
+        
+        ## write to dict
+        results[f"TP_{cls}"] = tp
+        results[f"FP_{cls}"] = fp
+        results[f"FN_{cls}"] = fn
+        results[f"TN_{cls}"] = tn
+        
+        ## comp seg metrics if any examples of the class are available in either pred or gt. This check saves a lot of time for TNs of which we have a lot with 50 classes
+        if is_in_pred or is_in_gt:
+            results[f"DICE_{cls}"] = dice_fn(gt==cls, predictions==cls)
+            results[f"HD95_{cls}"] = hd_fn(gt==cls, predictions==cls, 95, True)
+            results[f"VOLSIM_{cls}"] = volsim_fn(gt==cls, predictions==cls)
+        else: ##NOTE these are true negatives, this means all segmentations are correct and should technically be DSC=1, VS=1, HD95=0, but this would really bias the evaluation, so 0s are counted and the values are only averaged for TP, FP and FN!
+            results[f"DICE_{cls}"] = 0
+            results[f"HD95_{cls}"] = 0
+            results[f"VOLSIM_{cls}"] = 0
+
+
+            
+        
+    # results["INSTANCE_LEVEL"] = evaluation_function_instance_level(predictions, filename) omit for now.
+
+    return results
+    
+
+def evaluation_aggregation(metrics: List[dict], eps: float=1e-6) -> dict: # epsilons to avoid div by 0 error
+    """Aggregates the metrics over all samples and computes the per class Precision, Recal and MCC and normalizes the Dice Score, Volumetric Similarity and HD95 by the total amount of all TP and FN for each class.
+
+    Args:
+        metrics (List[dict]): The metrics for each sample
+        eps (float, optional): The epsilon to avoid division by zero. Defaults to 1e-6.
+
+    Returns:
+        dict: The aggregated metrics for each class
+    """
+    aggregates = {}
+    keys = metrics[0].keys()
+    ## aggregate all tpfpfn
+    for k in keys:
+        if k == 'INSTANCE_LEVEL':continue
+        aggregates[k]=sum([samp[k] for samp in metrics])
+    ## compute per location prec, rec, mcc
+    for i in range(1, N_CLASSES+1):
+        
+        # comp cls scores
+        aggregates[f"PRECISION_{i}"] = aggregates[f"TP_{i}"]/(aggregates[f"TP_{i}"]+aggregates[f"FP_{i}"]+eps)
+        aggregates[f"RECALL_{i}"] = aggregates[f"TP_{i}"]/(aggregates[f"TP_{i}"]+aggregates[f"FN_{i}"]+eps)
+        mcc_num = aggregates[f"TP_{i}"]*aggregates[f"TN_{i}"] - aggregates[f"FN_{i}"]*aggregates[f"FP_{i}"]
+        mcc_den = math.sqrt(
+            (aggregates[f"TP_{i}"]+aggregates[f"FP_{i}"])
+            *
+            (aggregates[f"TP_{i}"]+aggregates[f"FN_{i}"])
+            *
+            (aggregates[f"TN_{i}"]+aggregates[f"FP_{i}"])
+            *
+            (aggregates[f"TN_{i}"]+aggregates[f"FN_{i}"])
+        ) + eps
+        aggregates[f"MCC_{i}"] = mcc_num/mcc_den
+        
+        # comp segmentation scores
+        aggregates[f"DICE_{i}"] /= (aggregates[f"TP_{i}"]+aggregates[f"FN_{i}"]+aggregates[f"FP_{i}"]+eps)
+        aggregates[f"HD95_{i}"] /= (aggregates[f"TP_{i}"]+aggregates[f"FN_{i}"]+aggregates[f"FP_{i}"]+eps)
+        aggregates[f"VOLSIM_{i}"] /= (aggregates[f"TP_{i}"]+aggregates[f"FN_{i}"]+aggregates[f"FP_{i}"]+eps)
+    return aggregates
+
+def evaluation_average(metrics: dict) -> dict:
+    """Computes the average metrics across classes for ranking
+
+    Args:
+        metrics (dict): The per-class metrics
+
+    Returns:
+        dict: The average-across-classes metrics
+    """
+    averages = {"PRECISION": 0, "RECALL":0, "MCC":0, "DICE":0, "HD95":0, "VOLSIM":0}
+    for i in range(1, N_CLASSES+1):
+        averages["PRECISION"]+=metrics[f"PRECISION_{i}"]
+        averages["RECALL"]+=metrics[f"RECALL_{i}"]
+        averages["MCC"]+=metrics[f"MCC_{i}"]
+        averages["DICE"]+=metrics[f"DICE_{i}"]
+        averages["HD95"]+=metrics[f"HD95_{i}"]
+        averages["VOLSIM"]+=metrics[f"VOLSIM_{i}"]
+    return {k:v/N_CLASSES for k, v in averages.items()}
 
 class TopAneu26LikeEvaluator():
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, pipeline):
+        self.pipeline = pipeline
+        
+    def eval(self, testset):
+        results = []
+        for i in range(len(testset)):
+            smp = testset[i]
+            pred = self.pipeline(smp)
+            results.append(evaluation_function(pred, smp['location_mask']))
+        aggregates = evaluation_aggregation(results)
+        averages = evaluation_average(aggregates)
+        print('Results:')
+        pprint(averages)
+        return averages
--- TnT/pipeline/inference.py
+++ TnT/pipeline/inference.py
@@ -1,5 +1,101 @@
-
+from TnT.utils.transforms import get_train_test_transforms, DecodeTarget
+import SimpleITK as sitk, numpy as np, torch
+from scipy.ndimage import label
 
 class InferencePipeline():
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, model, patch_size_vx=64, patch_size_mm=35, device='cuda'):
+        self.s2_model = model
+        self.patch_size_mm = patch_size_mm
+        _, self.s2_transforms = get_train_test_transforms(patch_size_vx)
+        self.decoder = DecodeTarget()
+        self.device = device
+        self.s2_model.to(self.device)
+        self.s2_model.eval()
+        
+    def __call__(self, sample: sitk.Image, modality: str):
+        if isinstance(sample, sitk.Image): 
+            spacing = sample.GetSpacing()
+            stack = torch.stack(self._stage1(sample), dim=0).numpy()
+        elif isinstance(sample, dict):
+            spacing = sample['spacing']
+            stack = [sample['image'], sample['location_mask']!=0, sample['vessel_mask']!=0] ## fallback for s2 development purposes
+            stack = np.stack(stack, axis=0)
+            
+        cc, n = label(stack[2]) # labels the vessel mask channel
+        if n == 0: # early exit if no segmentation results
+            print('No aneurysms segmented')
+            return np.zeros_like(stack[2])
+        
+        output = np.zeros_like(stack[2]) # the image to write the things to
+        for comp in range(1, n+1):
+            cur_stack = stack.copy()
+            cur_stack[2] = cc==comp
+            predicted_label = self._stage2(cur_stack, spacing, modality)
+            print(predicted_label)
+    
+        
+    def _stage1(self, sample: sitk.Image):
+        raise NotImplementedError('Needs to return a tuple of (\n   image: (as it was passed to the funciton, no mutations or transformations applied), \n    location_mask: (binary), \n    vessel_mask: (binary)\n)')
+    
+    def _stage2(self, sample, spacing, modality):
+        # centroid in image space
+        centroid = np.mean(np.argwhere(sample[2]), axis=0).tolist()
+        
+        # vessel bbox
+        vbb_coords = np.argwhere(sample[1]) # VBB = Vessel Bounding Box
+        vbb_d = [int(np.min(vbb_coords[0])), int(np.max(vbb_coords[0]))]
+        vbb_h = [int(np.min(vbb_coords[1])), int(np.max(vbb_coords[1]))]
+        vbb_w = [int(np.min(vbb_coords[2])), int(np.max(vbb_coords[2]))]
+        vbb = [vbb_d, vbb_h, vbb_w]
+        
+        # crop in image space
+        cropped_stack = []
+        for i in range(3):
+            cropped_stack.append(self._center_crop(sample[i], centroid, spacing, self.patch_size_mm))
+        cropped_stack = np.stack(cropped_stack, axis=0)
+        
+        # translate centorid to vessel space for inference
+        coords_in_vbb = [
+            centroid[0]-vbb[0][0],
+            centroid[1]-vbb[1][0],
+            centroid[2]-vbb[2][0]
+        ]
+        
+        # compute the thang
+        pred_lat, pred_loc = self.s2_model.classify(torch.from_numpy(cropped_stack).unsqueeze(0).to(self.device), torch.tensor(coords_in_vbb).unsqueeze(0).to(self.device), [modality])
+        pred_lat=pred_lat.detach().to('cpu')
+        pred_loc=pred_loc.detach().to('cpu')
+        
+        decoded_label = self.decoder(torch.concat([pred_loc, pred_lat], dim=-1))
+        return decoded_label
+
+    def _center_crop(self, img, coords, spacing, size_mm):
+            sz_mm = round(size_mm/2)
+            sz = [round(sz_mm/sp) for sp in spacing]
+            shape = img.shape
+            
+            coords_lower = []
+            coords_upper = []
+            for c_ax, sh_ax, sz_ax in zip(coords, shape, sz):
+    
+                upper = c_ax+sz_ax
+                lower = c_ax-sz_ax
+                
+                if upper<=sh_ax and lower >=0:
+                    coords_lower.append(lower)
+                    coords_upper.append(upper)
+                elif upper > sh_ax:
+                    diff = upper-sh_ax
+                    coords_upper.append(sh_ax)
+                    coords_lower.append(lower-diff)
+                elif lower < 0:
+                    diff = lower
+                    coords_lower.append(0)
+                    coords_upper.append(upper+abs(diff))
+            
+            crop = img[
+                int(coords_lower[0]):int(coords_upper[0]),
+                int(coords_lower[1]):int(coords_upper[1]),
+                int(coords_lower[2]):int(coords_upper[2])
+            ]            
+            return crop
