46a70a0e4868f107534e7f53c3ce3245af10e7b6
Lorenz Achim Kuhn <l.a.kuhn@tue.nl>
2026-08-17 12:52:43 +0200

    minor

--- inference.py
+++ inference.py
@@ -7,14 +7,12 @@ from TnT.pipeline.inference import InferencePipeline
 if __name__ == '__main__':
     PATCH_SIZE_VX = 64 # to avoid oom error on local
     PATCH_SIZE_MM = 35
-    BATCH_SIZE = 4
-    EARLY_STOP_PATCHING = False
     
     
     
     ## Prep model
     model = TnTS2()
-    model.load('/home/tue20260926/Repos/topaneu_tulips_and_tapas/_pretrain/new_architecture/new')
+    model.load('/home/tue20260926/Repos/topaneu_tulips_and_tapas/_tune/TnTS2_training_from-10:17:54-17.08.26/best_val_loss')
     
     ## Prep pipeline
     pl = InferencePipeline(model, PATCH_SIZE_VX, PATCH_SIZE_MM)
