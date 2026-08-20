Fix VBB computation and inclue coordinate flipping in the RnadomFlipLat transform

TnT/pipeline/inference.py
@@ -8,12 +8,13 @@ from scipy.ndimage import label
 from pathlib import Path
 
 class InferencePipeline():
-    def __init__(self, s1_model_path, s2_model_path, patch_size_vx=64, patch_size_mm=35, device='cuda'):
+    def __init__(self, s1_model_path, s2_model_path, patch_size_vx=64, patch_size_mm=35, device='cuda', use_tta=True):
         
         self.patch_size_mm = patch_size_mm
         self.s2_transforms = get_inference_transforms(patch_size_vx)
         self.decoder = DecodeAneu()
         self.device = device
+        self.tta = use_tta
         
         ## the models
         if s1_model_path is not None: self.s1_model = self._make_s1(s1_model_path)
@@ -45,7 +46,8 @@ class InferencePipeline():
             s2ds = CaseDL(image, vmask, lmask, modality, self.s2_transforms, spacing, self.patch_size_mm)
             for i in range(len(s2ds)):
                 smp = s2ds[i]
-                pred = self._stage2(smp)
+                if self.tta: pred = self._stage2_TTA(smp)
+                else: pred = self._stage2(smp)
                 s2ds.add_label(i, pred)
             output = s2ds.make_mask()
         else: output = np.zeros_like(vmask)
@@ -83,6 +85,32 @@ class InferencePipeline():
         assert len(preds.shape)==2, 'only implemented for batched data'
         decoded_label = self.decoder(preds)[0][2]
         return decoded_label
+    
+    def _stage2_TTA(self, smp:dict) -> int:
+        ttas = [
+            (False, False, False), 
+            (True, False, False), 
+            (False, True, False), 
+            (False, False, True),
+            (True, True, False),
+            (True, False, True),
+            (False, True, True),
+            (True, True, True),
+        ]
+        
+        loc_probas = []
+        lat_probas = []
+        for tta in ttas:
+            img = smp['image']
+            crd = smp['coords']
+            
+            flip_lat = False
+            for dim, trig in enumerate(tta):
+                
+            pred_lat, pred_loc = self.s2_model.classify(img.unsqueeze(0).to(self.device), crd.unsqueeze(0).to(self.device), [smp['modality']])
+            pred_lat=pred_lat.detach().to('cpu')
+            pred_loc=pred_loc.detach().to('cpu')
+            
     
     def _make_s1(self, path):
         return get_s1(path, self.device)

TnT/utils/dataloader.py
@@ -235,9 +235,9 @@ class TopAneu_TnTs2_DS(Dataset):
             seed = possible_seeds[random.choice(range(possible_seeds.shape[0])), :]
             
             vbb_coords = np.argwhere(img_smp['vessel_mask']) # VBB = Vessel Bounding Box
-            vbb_d = [int(np.min(vbb_coords[0])), int(np.max(vbb_coords[0]))]
-            vbb_h = [int(np.min(vbb_coords[1])), int(np.max(vbb_coords[1]))]
-            vbb_w = [int(np.min(vbb_coords[2])), int(np.max(vbb_coords[2]))]
+            vbb_d = [int(np.min(vbb_coords[:, 0])), int(np.max(vbb_coords[:, 0]))]
+            vbb_h = [int(np.min(vbb_coords[:, 1])), int(np.max(vbb_coords[:, 1]))]
+            vbb_w = [int(np.min(vbb_coords[:, 2])), int(np.max(vbb_coords[:, 2]))]
             
             smp = {
                     'idx': i, # the base image idx in the base dataset
@@ -317,9 +317,9 @@ class TopAneu_TnTs2_DS(Dataset):
             cc, n = label(sample['location_mask'])
             
             vbb_coords = np.argwhere(sample['vessel_mask']) # VBB = Vessel Bounding Box
-            vbb_d = [int(np.min(vbb_coords[0])), int(np.max(vbb_coords[0]))]
-            vbb_h = [int(np.min(vbb_coords[1])), int(np.max(vbb_coords[1]))]
-            vbb_w = [int(np.min(vbb_coords[2])), int(np.max(vbb_coords[2]))]
+            vbb_d = [int(np.min(vbb_coords[:, 0])), int(np.max(vbb_coords[:, 0]))]
+            vbb_h = [int(np.min(vbb_coords[:, 1])), int(np.max(vbb_coords[:, 1]))]
+            vbb_w = [int(np.min(vbb_coords[:, 2])), int(np.max(vbb_coords[:, 2]))]
             
             for obj in range(1, n+1):
                 smp = {
@@ -616,9 +616,9 @@ class TopAneu_TnTs2_DS_for_vessel_pt(Dataset):
             vessel_mask = sample['vessel_mask']
             
             vbb_coords = np.argwhere(vessel_mask) # VBB = Vessel Bounding Box
-            vbb_d = [int(np.min(vbb_coords[0])), int(np.max(vbb_coords[0]))]
-            vbb_h = [int(np.min(vbb_coords[1])), int(np.max(vbb_coords[1]))]
-            vbb_w = [int(np.min(vbb_coords[2])), int(np.max(vbb_coords[2]))]
+            vbb_d = [int(np.min(vbb_coords[:, 0])), int(np.max(vbb_coords[:, 0]))]
+            vbb_h = [int(np.min(vbb_coords[:, 1])), int(np.max(vbb_coords[:, 1]))]
+            vbb_w = [int(np.min(vbb_coords[:, 2])), int(np.max(vbb_coords[:, 2]))]
             
             vessel_cls = np.unique(vessel_mask).tolist()
             

TnT/utils/transforms/misc.py
@@ -421,8 +421,9 @@ class RandomFlipLaterality():
     
     def __call__(self, dct):
         if self.execute: 
+            dct['coords'][2] = 1-dct['coords'][2]
             if dct['image'].dim() == 3:
-                dct['image'] = torch.flip(dct['image'], dims=[self.aterality_dimension])
+                dct['image'] = torch.flip(dct['image'], dims=[self.laterality_dimension])
                 dct['laterality'] = self._flip_laterality(dct['laterality'])
             
             else:

misc.py
@@ -33,13 +33,16 @@ if __name__ == '__main__':
     train_transforms = None
     
     ## load splits
-    if not os.path.exists('tuning-train.json'):
-        train = TopAneu_TnTs2_DS.load('train.json', None)
+    if not os.path.exists('tuning-val.json'):
+        train = TopAneu_TnTs2_DS.load('val.json', None)
         train.preprocess(include_bg=0.2, max_items=1 if EARLY_STOP_PATCHING else -1)
-        train.save('tuning-train.json')
-    else: train = TopAneu_TnTs2_DS.load('tuning-train.json', None)
+        train.save('tuning-val.json')
+    else: train = TopAneu_TnTs2_DS.load('tuning-val.json', None)
     
-    AdaNorm.compute_mean_std(train.image_ds)
+    
+    
+    for i in range(len(train)):
+        print(train[i]['coords'])
     
     
     # cnt = np.zeros(29)