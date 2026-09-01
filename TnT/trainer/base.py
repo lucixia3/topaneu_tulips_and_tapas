5eee70d71a33a5f76c3ecabeaa560cd16b83bc24
Lorenz Achim Kuhn <l.a.kuhn@tue.nl>
2026-09-01 10:28:02 +0200

    remove eval on precomp-pred

--- TnT/trainer/base.py
+++ TnT/trainer/base.py
@@ -151,15 +151,15 @@ class BasicTrainer():
         with open(f'{outdir}/per_cls.json', 'w') as f:
             json.dump(avg, f, indent=4)
         
-        print('----------- With Stage 1 Segmentations ----------')
-        pl = InferencePipeline(s1, model, 64, 35, use_tta=True)
-        outdir = self.wdir/'eval'/'with_s1'
-        ev = TopAneu26LikeEvaluator(pl, outdir, use_perfect_segmentations=False)
-        res, agg, avg, disc = ev.eval_ds(testds)#ev.eval_list(test.src, test.cases)
-        with open(f'{outdir}/classification_failure.json', 'w') as f:
-                json.dump(disc, f, indent=4)
-        os.makedirs(outdir, exist_ok=True)
-        ev.plot(agg, f'{outdir}/per_cls')
-        ev.re_eval_by_modality(res, outdir)
-        with open(f'{outdir}/per_cls.json', 'w') as f:
-            json.dump(avg, f, indent=4)
+        # print('----------- With Stage 1 Segmentations ----------')
+        # pl = InferencePipeline(s1, model, 64, 35, use_tta=True)
+        # outdir = self.wdir/'eval'/'with_s1'
+        # ev = TopAneu26LikeEvaluator(pl, outdir, use_perfect_segmentations=False)
+        # res, agg, avg, disc = ev.eval_ds(testds)#ev.eval_list(test.src, test.cases)
+        # with open(f'{outdir}/classification_failure.json', 'w') as f:
+        #         json.dump(disc, f, indent=4)
+        # os.makedirs(outdir, exist_ok=True)
+        # ev.plot(agg, f'{outdir}/per_cls')
+        # ev.re_eval_by_modality(res, outdir)
+        # with open(f'{outdir}/per_cls.json', 'w') as f:
+        #     json.dump(avg, f, indent=4)
